3107fbb9f9e9f471738377b72eccd4d5a4e7b2f2
liuzxc <lxq_102172@163.com>
2016-06-17 14:05:19 +0800

    save

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -8,7 +8,7 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/Users/liuxingqi/meiqia/ecoboost/spark-1.6.1/README.md" // Should be some file on your system
+    val logFile = "/Users/liuxingqi/spark/spark-1.6.1/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
